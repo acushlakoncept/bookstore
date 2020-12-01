2ba6331c124cb2000f1f1bbcdf6ce67c6005effc
Uduak Essien <acushla4real@gmail.com>
2020-12-01 06:17:55 +0100

    add axios and fetchCategories action using axios

--- src/redux/category/categoryActions.js
+++ src/redux/category/categoryActions.js
@@ -1,22 +1,33 @@
-import * as actions from './categoryTypes';
+import axios from 'axios';
+import {
+  FETCH_CATEGORY,
+  FETCH_CATEGORY_SUCCESS,
+  FETCH_CATEGORY_FAILURE,
+} from './categoryTypes';
 
-export const fetchCategoryRequest = () => {
-  return {
-    type: actions.FETCH_CATEGORY
-  };
+export const fetchCategoryRequest = () => ({
+  type: FETCH_CATEGORY,
+});
+
+export const fetchCategorySuccess = category => ({
+  type: FETCH_CATEGORY_SUCCESS,
+  payload: category,
+});
+
+export const fetchCategoryFailure = error => ({
+  type: FETCH_CATEGORY_FAILURE,
+  payload: error,
+});
+
+export const fetchCategories = () => function (dispatch) {
+  dispatch(fetchCategoryRequest());
+  axios
+    .get('https://cush-bookstore-api.herokuapp.com/api/v1/category')
+    .then(response => {
+      const categories = response.data;
+      dispatch(fetchCategorySuccess(categories));
+    })
+    .catch(error => {
+      dispatch(fetchCategoryFailure(error.message));
+    });
 };
-
-export const fetchCategorySuccess = (category) => {
-  return {
-    type: actions.FETCH_CATEGORY_SUCCESS,
-    payload: category
-  };
-};
-
-export const fetchCategoryFailure = (error) => {
-  return {
-    type: actions.FETCH_CATEGORY_FAILURE,
-    payload: error
-  };
-};
-
