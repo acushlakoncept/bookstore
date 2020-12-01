6afd71f9eeba125cca69d69db632470013442d3c
Uduak Essien <acushla4real@gmail.com>
2020-12-01 04:54:29 +0100

    add action types and action creators for getting books

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
-import { CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER } from './actionTypes';
+import * as actions from './actionTypes';
 
 export const addBook = (title, category) => ({
-  type: CREATE_BOOK,
+  type: actions.CREATE_BOOK,
   payload: {
     title,
     category,
@@ -9,14 +9,54 @@ export const addBook = (title, category) => ({
 });
 
 export const removeBook = id => ({
-  type: REMOVE_BOOK,
+  type: actions.REMOVE_BOOK,
   payload: {
     id,
   },
 });
 
+export const fetchBooksRequest = () => {
+  return {
+    type: actions.FETCH_BOOKS
+  };
+};
+
+export const fetchBooksSuccess = (books) => {
+  return {
+    type: actions.FETCH_BOOKS_SUCCESS,
+    payload: books
+  };
+};
+
+export const fetchBooksFailure = (error) => {
+  return {
+    type: actions.FETCH_BOOKS_FAILURE,
+    payload: error
+  };
+};
+
+export const fetchCategoryRequest = () => {
+  return {
+    type: actions.FETCH_CATEGORY
+  };
+};
+
+export const fetchCategorySuccess = (category) => {
+  return {
+    type: actions.FETCH_CATEGORY_SUCCESS,
+    payload: category
+  };
+};
+
+export const fetchCategoryFailure = (error) => {
+  return {
+    type: actions.FETCH_CATEGORY_FAILURE,
+    payload: error
+  };
+};
+
 export const changeFilter = category => ({
-  type: CHANGE_FILTER,
+  type: actions.CHANGE_FILTER,
   payload: {
     category,
   },
